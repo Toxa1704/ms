fa89525fa6d3b3d6952bfd88b2e204d0de48fde5
Toxa1704 <Toxa1704@outlook.com>
2020-11-28 17:45:43 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    $('.slider').slick();
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000
+    });
 });
